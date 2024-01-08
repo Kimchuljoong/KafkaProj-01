4c5f5dc9f1ce29b736dbd07a588cc3eed716d39b
kimchj <kimchj@LM-045335-00.local>
2024-01-08 15:26:29 +0900

    rebalancing strategy

--- consumers/src/main/java/com/example/kafka/ConsumerMTopicRebalance.java
+++ consumers/src/main/java/com/example/kafka/ConsumerMTopicRebalance.java
@@ -1,9 +1,6 @@
 package com.example.kafka;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.*;
 import org.apache.kafka.common.errors.WakeupException;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
@@ -13,25 +10,24 @@ import java.time.Duration;
 import java.util.List;
 import java.util.Properties;
 
-public class ConsumerWakeup {
+public class ConsumerMTopicRebalance {
 
-    public static final Logger logger = LoggerFactory.getLogger(ConsumerWakeup.class.getName());
+    public static final Logger logger = LoggerFactory.getLogger(ConsumerMTopicRebalance.class.getName());
 
     public static void main(String[] args) {
-
-        String topicName = "simple-topic";
 
         Properties props = new Properties();
         props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.56.101:9092");
         props.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         props.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "group-assign");
 //        props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "group_01");
-        props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "group_01_static");
-        props.setProperty(ConsumerConfig.GROUP_INSTANCE_ID_CONFIG, "1");
-
+//        props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "group-mtopic");
+//        props.setProperty(ConsumerConfig.GROUP_INSTANCE_ID_CONFIG, "1");
+        props.setProperty(ConsumerConfig.PARTITION_ASSIGNMENT_STRATEGY_CONFIG, RoundRobinAssignor.class.getName());
 
         KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<String, String>(props);
-        kafkaConsumer.subscribe(List.of(topicName));
+        kafkaConsumer.subscribe(List.of("topic-p3-t1", "topic-p3-t2"));
 
         Thread mainThread = Thread.currentThread();
 
@@ -53,8 +49,8 @@ public class ConsumerWakeup {
             while (true) {
                 ConsumerRecords<String, String> consumerRecords = kafkaConsumer.poll(Duration.ofMillis(1000L));
                 for (ConsumerRecord record : consumerRecords) {
-                    logger.info("record key: {}, record value: {} partition: {}",
-                            record.key(), record.value(), record.partition());
+                    logger.info("topic:{}, record key: {}, record value: {} partition: {}",
+                            record.topic(), record.key(), record.value(), record.partition());
                 }
             }
         } catch (WakeupException e) {
